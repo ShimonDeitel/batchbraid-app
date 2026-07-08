8fbd711f0c5c12a8801c7f6554070552021d06df
rex-del <150361329+rex-del@users.noreply.github.com>
2026-07-08 22:26:59 +0300

    Fix duplicate property/init param in model struct

--- iOS/BatchBraid/Models.swift
+++ iOS/BatchBraid/Models.swift
@@ -8,14 +8,7 @@ struct Braid: Identifiable, Codable, Equatable {
     var finishedSize: String
     var createdDate: Date
 
-    init(
-        id: UUID = UUID(),
-        projectName: String = "Keychain Fob",
-        cordLength: String = "6",
-        pattern: String = "Cobra",
-        finishedSize: String = "4",
-        createdDate: Date = Date()
-    ) {
+    init(id: UUID = UUID(), projectName: String = "Keychain Fob", cordLength: String = "6", pattern: String = "Cobra", finishedSize: String = "4", createdDate: Date = Date()) {
         self.id = id
         self.projectName = projectName
         self.cordLength = cordLength
@@ -34,14 +27,7 @@ struct BBProEntry: Identifiable, Codable, Equatable {
     var resultLength: String
     var createdDate: Date
 
-    init(
-        id: UUID = UUID(),
-        pattern: String = "Cobra",
-        targetLength: String = "8",
-        strands: String = "2",
-        resultLength: String = "7.5",
-        createdDate: Date = Date()
-    ) {
+    init(id: UUID = UUID(), pattern: String = "Cobra", targetLength: String = "8", strands: String = "2", resultLength: String = "7.5", createdDate: Date = Date()) {
         self.id = id
         self.pattern = pattern
         self.targetLength = targetLength
